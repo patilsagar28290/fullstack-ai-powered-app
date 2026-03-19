26544806ab87c67ded5e57814c766b46e921e384
patilsagar28290 <patilsagar28290@gmail.com>
2026-03-19 20:56:07 +0530

    connect frontend and backend

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react"
+
+
+function App() {
+
+  const [msg, setMsg] = useState('')
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(res => res.json())
+      .then(data => setMsg(data.message))
+  }, []);
+  
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>{msg}</p>
+    </div>
+  )
+}
+
+export default App
---
+++ packages/server/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+const env_var = dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.get('/', (req: Request, res: Response) => {
+  res.send(env_var.parsed?.OPENAI_API_KEY ? `API Key is set` : 'API Key is not set');
+});
+
+app.get('/api/hello', (req: Request, res: Response) => {
+  res.json({ message: 'Hello from the server!' });
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
