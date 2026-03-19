21ac92f55c4d55fc204fc425d686b16d9b7fe987
patilsagar28290 <patilsagar28290@gmail.com>
2026-03-19 21:03:25 +0530

    concurrently run client and server with root bun run dev cmd

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log("Hello via Bun!");
+import concurrently from "concurrently";
+
+concurrently([
+    { name: "server", cwd: "./packages/server", command: "bun --watch run dev", prefixColor: "blue" },
+    { name: "client", cwd: "./packages/client", command: "bun --watch run dev", prefixColor: "green" }
+]);
