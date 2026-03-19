f9ca731ac1fe099fd79da43be9c46d5319de0dd8
patilsagar28290 <patilsagar28290@gmail.com>
2026-03-19 21:55:36 +0530

    setup husky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,16 +2,14 @@ import { useEffect, useState } from 'react';
 
 function App() {
    const [msg, setMsg] = useState('');
-
    useEffect(() => {
       fetch('/api/hello')
          .then((res) => res.json())
          .then((data) => setMsg(data.message));
-   }, []);
-
+   }, [])
    return (
       <div className="p-4">
-         <p className="font-bold p-4 text-3xl">{msg}</p>
+<p className="font-bold p-4 text-3xl">{msg}</p>
          <button
             className="bg-blue-500 text-white px-4 py-2 rounded"
             onClick={() => alert('Button clicked!')}
@@ -21,5 +19,4 @@ function App() {
       </div>
    );
 }
-
 export default App;
